e3e9c476e58df0e430e65424338d1ad39d9aef07
hahatula <olgagolubev.pro@gmail.com>
2024-04-30 16:52:44 +0300

    import router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const router = require('express').Router();
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
