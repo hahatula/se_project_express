1e85a701f57c47ff7228b60c8a18cb55984fdf3f
hahatula <olgagolubev.pro@gmail.com>
2024-05-06 18:56:35 +0300

    256-bit key

--- utils/config.js
+++ utils/config.js
@@ -1,1 +1,3 @@
-module.exports.JWT_SECRET = "some-secret-key";
+module.exports.JWT_SECRET = "920d62dfbebc4a49fbc1581a2a667a93f29fb01c8972ebbba663315b75891f5f";
+// command to create a secret:
+// node -e "console.log(require('crypto').randomBytes(32).toString('hex'));"
