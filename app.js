479e9a0183a022ae63f46e4df5d49bbfb58225d4
hahatula <olgagolubev.pro@gmail.com>
2024-06-12 12:42:32 +0300

    improve security

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-require('dotenv').config();
+require("dotenv").config();
 const cors = require("cors");
 const { errors } = require("celebrate");
+const helmet = require("helmet");
 const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
+const { limiter } = require("./middlewares/limiter");
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 const { PORT = 3001 } = process.env;
 const app = express();
 
-app.get('/crash-test', () => {
+app.use(helmet());
+app.get("/crash-test", () => {
   setTimeout(() => {
-    throw new Error('Server will crash now');
+    throw new Error("Server will crash now");
   }, 0);
 });
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
+app.use(limiter);
 app.use("/", require("./routes/index"));
 
 app.use(errorLogger);
